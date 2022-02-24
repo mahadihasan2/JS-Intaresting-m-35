a46cc1d5f2a59bae51b707d0e24dcf04812851f6
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-24 23:00:55 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,35 @@
+// Falshy --------------------
+/* 
+false 
+0 is false
+eamty string is falscy
+undifiend
+null 
+NaN
+
+*/
+
+
+// Falsehy --------------------
+
+// Truthy===========================
+/* 
+true
+any number(positive or nagetive) is true
+any string including whiteSpace is  and '0' truthy
+[]
+{}
+anything else is not falscy will be truthy
+
+*/
+
+// Truthy===========================
+
+
+let x = NaN
+console.log('value of x:', x)
+if (x) {
+    console.log('variable is truthy')
+} else {
+    console.log('variable is falshy')
+}
