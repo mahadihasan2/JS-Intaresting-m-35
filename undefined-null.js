4a41e1fba350be2b42b396250b5b667b4a28aac8
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-25 00:55:09 +0600

    undefined-null-js

---
+++ undefined-null.js
@@ -0,0 +1,31 @@
+let sum
+// console.log(sum)
+function add(a, b) {
+    const result = a + b
+    return;
+    return result
+
+}
+const parameter = add(2, 4)
+// console.log(parameter)
+
+function sumCalletion(a, b) {
+    const result = a + 43
+    const result2 = result * 54
+    return b
+}
+const parameter2 = sumCalletion(43)
+// console.log(parameter2)
+
+const student = {
+    name: 'Mahadi',
+    job: 'Web Developer',
+    salary: 6734
+}
+// console.log(student.location)
+
+const array = [65, 78, 34, 21]
+// console.log(array[5])
+
+const value = undefined
+console.log(value)
