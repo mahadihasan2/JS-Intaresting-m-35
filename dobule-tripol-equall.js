c7d3d7568d178219c3be2f84abed8fd5d7e53a63
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-25 01:14:49 +0600

    dobule-tripol-equall

---
+++ dobule-tripol-equall.js
@@ -0,0 +1,15 @@
+const add1 = 20
+const add2 = '20'
+if (add1 == add2) {
+    console.log('addition is true')
+} else {
+    console.log('addition is not true')
+}
+
+const sum1 = []
+const sum2 = []
+if (sum1 === sum2) {
+    console.log('suamation is true')
+} else {
+    console.log('sumation is not true')
+}
