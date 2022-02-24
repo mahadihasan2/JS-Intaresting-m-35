9ba59d886dbff2f2505ffe1fe0dfde0bba1d61b9
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-24 21:58:17 +0600

    primitive and nonPrimitive

---
+++ primitive.js
@@ -0,0 +1,19 @@
+let a = 'Hello'
+let b = a;
+console.log(a, b)
+a = 'gello'
+console.log(a, b)
+
+const student1 = {
+
+
+    job: 'web developer',
+
+
+
+}
+const student2 = student1
+console.log(student1, student2)
+
+student1.job = 'Font and developer'
+console.log(student1, student2)
