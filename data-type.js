3ea06af6708c2bfff0bbc06f13fa1d23803180ee
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-24 20:40:31 +0600

    js-intaresting-1st

---
+++ data-type.js
@@ -0,0 +1,8 @@
+const a = 33; // number
+const age = 34; // Number
+
+let a = 45; // Number Type data
+// console.log(typeof a);
+let a = 'Hi i am Mahadi Hasan' // String type data type
+let a = false / true // Boolean Type data
+console.log(typeof a)
