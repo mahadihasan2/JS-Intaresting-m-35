f7be6fdba49a569b366ad3f734cf5a05b240b501
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-25 17:50:23 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+const favNum = 23;
+
+function add(frist, seconed) {
+    const result = frist + seconed
+    /* console.log(result)
+    console.log(favNum) */
+    if (result > 9) {
+        // const mood = 'happy'
+        // let mood = 'happy'
+        var mood = 'happy'
+        // console.log(mood)
+    }
+    console.log(mood)
+    return result
+
+}
+const sum = add(35, 6)
+// console.log(frist) 
